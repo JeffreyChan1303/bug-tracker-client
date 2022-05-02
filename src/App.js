35b83e0881ab4fb02c2b1586188a37b62a7779ba
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2022-05-02 15:51:46 -0400

    initialized expected file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+
+import { Navbar, Header, Dashboard, Projects, Tickets} from './components';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
+        <div className="app">
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="header">
+                <Header />
+            </div>
+            <div className="main">
+                <Container >
+                    {/* Place holder for all the routes */}
+                </Container>
+            </div>
         </div>
     )
 }
---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar} from '@mui/material';
+import { Link } from 'react-router-dom';
+
+
+const Navbar = () => {
+    return (
+        <div className="nav-container">
+            <div className="logo-container">
+                <Typography variant="h1">
+                    THIS WORKS!
+                </Typography>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;

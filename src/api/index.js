a9cc1df6c30ab25fa06d776743bb51442e47820a
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2022-06-24 17:44:43 -0400

    fixed incorrect getAllUsersBySearch api call

--- src/api/index.js
+++ src/api/index.js
@@ -15,7 +15,7 @@ API.interceptors.request.use((req) => {
 export const signIn = (formData) => API.post('/users/signin', formData);
 export const signUp = (formData) => API.post('/users/signup', formData);
 export const getAllUsers = (page) => API.get(`/users/allUsers?page=${page}`);
-export const getAllUsersBySearch = (page) => API.get(`/users/allUsers?page=${page}`);
+export const getAllUsersBySearch = (page, searchQuery) => API.get(`/users/allUsers/search?searchQuery=${searchQuery || 'none'}&page=${page}`);
 
 // Ticket APIs
 export const getAllTickets = (page) => API.get(`/tickets/allTickets?page=${page}`);

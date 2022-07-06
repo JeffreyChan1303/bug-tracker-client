ed52cbda36487f90159652c223a97ede2fc1a4e3
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2022-07-06 17:41:05 -0400

    added 4 dashboard.jsx cards.

--- src/components/Application/Dashboard/dashboard.jsx
+++ src/components/Application/Dashboard/dashboard.jsx
@@ -1,41 +1,114 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Grid, Card, Box, Avatar } from '@mui/material';
 
 const Dashboard = () => {
 
     return (
         <>
-            <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-            enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-            imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-            Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-            Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-            nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-            leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-            feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-            sapien faucibus et molestie ac.
+            <Typography variant="h4">
+                Dashboard
             </Typography>
 
-            {[1,2,3,4,5].map((element, index) => (
-                <Typography paragraph key={element}>
-                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                posuere sollicitudin aliquam ultrices sagittis orci a.
-                </Typography>
-            ))}
-    </>
+            <Grid container spacing={2}>
+                {/* Active Projects card */}
+                <Grid item xs={12} md={6} lg={3} >
+                    <Card elevation={1} sx={{ p: 3 }}  >
+                        <Grid container justifyContent="space-between">
+                            <Grid item >
+                                <Typography variant="body1">
+                                    Active Projects
+                                </Typography>
+                                <Typography variant="h6">
+                                    (number)
+                                </Typography>
+                            </Grid>
+                            <Grid item >
+                                <Avatar />
+                            </Grid>
+                        </Grid>
+                        <Box >
+                            <Typography variant="caption" >
+                                this is just the bottom of the card
+                            </Typography>
+                        </Box>
+                    </Card>
+                </Grid>
+
+                {/* Total tickets card */}
+                <Grid item xs={12} md={6} lg={3} >
+                    <Card elevation={1} sx={{ p: 3 }}  >
+                        <Grid container justifyContent="space-between">
+                            <Grid item >
+                                <Typography variant="body1">
+                                    Total Tickets
+                                </Typography>
+                                <Typography variant="h6">
+                                    (number)
+                                </Typography>
+                            </Grid>
+                            <Grid item >
+                                <Avatar />
+                            </Grid>
+                        </Grid>
+                        <Box >
+                            <Typography variant="caption" >
+                                this is just the bottom of the card
+                            </Typography>
+                        </Box>
+                    </Card>
+                </Grid>
+
+                {/* Unassigned Tickets Card */}
+                <Grid item xs={12} md={6} lg={3} >
+                    <Card elevation={1} sx={{ p: 3 }}  >
+                        <Grid container justifyContent="space-between">
+                            <Grid item >
+                                <Typography variant="body1">
+                                    Unassigned Tickets
+                                </Typography>
+                                <Typography variant="h6">
+                                    (number)
+                                </Typography>
+                            </Grid>
+                            <Grid item >
+                                <Avatar />
+                            </Grid>
+                        </Grid>
+                        <Box >
+                            <Typography variant="caption" >
+                                this is just the bottom of the card
+                            </Typography>
+                        </Box>
+                    </Card>
+                </Grid>
+
+                {/* Unread Notifications card */}
+                <Grid item xs={12} md={6} lg={3} >
+                    <Card elevation={1} sx={{ p: 3 }}  >
+                        <Grid container justifyContent="space-between">
+                            <Grid item >
+                                <Typography variant="body1">
+                                    Unread Notifications
+                                </Typography>
+                                <Typography variant="h6">
+                                    (number)
+                                </Typography>
+                            </Grid>
+                            <Grid item >
+                                <Avatar />
+                            </Grid>
+                        </Grid>
+                        <Box >
+                            <Typography variant="caption" >
+                                this is just the bottom of the card
+                            </Typography>
+                        </Box>
+                    </Card>
+                </Grid>
+
+
+            </Grid>
+        </>
     )
 };
 
--- src/components/Application/Navbar/navbar.jsx
+++ src/components/Application/Navbar/navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Toolbar, Divider, List, ListItem, ListItemIcon, ListItemText, Typography, Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Link } from 'react-router-dom';
 
@@ -35,215 +34,202 @@ const Navbar = () => {
     
     return ( 
         <div>
-        <Toolbar>
-            <Typography variant="h5">Juicy Bug Tracker</Typography>
-        </Toolbar>
-        <Divider />
+            <Toolbar >
+                <Typography variant="h5">Juicy Bug Tracker</Typography>
+            </Toolbar>
+            <Divider sx={{ mb: "50px" }}/>
 
-        <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-                <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-            </ListItem>
-            ))}
-        </List>
+            <List>
+                <Divider />
+                <StyledLink to="/dashboard">
+                    <ListItem button key={"Dashboard"} >
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Dashboard" />
+                    </ListItem>
+                </StyledLink>
 
-        <List>
-            <Divider />
-            <StyledLink to="/dashboard">
-                <ListItem button key={"Dashboard"} >
-                    <ListItemIcon>
-                        <InboxIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Dashboard" />
-                </ListItem>
-            </StyledLink>
+                <Divider />
+                <StyledLink to="/notifications">
+                    <ListItem button key={"Notification Inbox"}>
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Notification Inbox" />
+                    </ListItem>
+                </StyledLink>
 
-            <Divider />
-            <StyledLink to="/notifications">
-                <ListItem button key={"Notification Inbox"}>
-                    <ListItemIcon>
-                        <InboxIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Notification Inbox" />
-                </ListItem>
-            </StyledLink>
+                <Divider />
 
-            <Divider />
+                {/* Nav Projects */}
+                <StyledAccordion 
+                    expanded={expanded === 'panel1'} 
+                    onChange={handleChange('panel1')} 
+                    disableGutters={true}
+                    elevation={0}
+                >
+                    <StyledAccordionSummary expandIcon={<ExpandMoreIcon />} >
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                            Projects 
+                        </Typography>
+                    </StyledAccordionSummary>
 
-            {/* Nav Projects */}
-            <StyledAccordion 
-                expanded={expanded === 'panel1'} 
-                onChange={handleChange('panel1')} 
-                disableGutters={true}
-                elevation={0}
-            >
-                <StyledAccordionSummary expandIcon={<ExpandMoreIcon />} >
-                    <ListItemIcon>
-                        <InboxIcon />
-                    </ListItemIcon>
-                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                        Projects 
-                    </Typography>
-                </StyledAccordionSummary>
+                    <AccordionDetails sx={{ padding: "0"}} >
+                        <StyledLink to="/allProjects">
+                            <ListItem button key={"All Projects"} sx={{ paddingLeft: "32px" }}>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="All Projects" />
+                            </ListItem>
+                        </StyledLink>
+                    </AccordionDetails>
 
-                <AccordionDetails sx={{ padding: "0"}} >
-                    <StyledLink to="/allProjects">
-                        <ListItem button key={"All Projects"} sx={{ paddingLeft: "32px" }}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="All Projects" />
-                        </ListItem>
-                    </StyledLink>
-                </AccordionDetails>
+                    <AccordionDetails sx={{ padding: "0"}} >
+                        <StyledLink to="/myProjects">
+                            <ListItem button key={"My Projects"} sx={{ paddingLeft: "32px" }}>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="My Projects" />
+                            </ListItem>
+                        </StyledLink>
+                    </AccordionDetails>
 
-                <AccordionDetails sx={{ padding: "0"}} >
-                    <StyledLink to="/myProjects">
-                        <ListItem button key={"My Projects"} sx={{ paddingLeft: "32px" }}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="My Projects" />
-                        </ListItem>
-                    </StyledLink>
-                </AccordionDetails>
+                    <AccordionDetails sx={{ padding: "0"}} >
+                        <StyledLink to="/addProject">
+                            <ListItem button key={"Add Project"} sx={{ paddingLeft: "32px" }}>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Add Project" />
+                            </ListItem>
+                        </StyledLink>
+                    </AccordionDetails>
 
-                <AccordionDetails sx={{ padding: "0"}} >
-                    <StyledLink to="/addProject">
-                        <ListItem button key={"Add Project"} sx={{ paddingLeft: "32px" }}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Add Project" />
-                        </ListItem>
-                    </StyledLink>
-                </AccordionDetails>
+                    <AccordionDetails sx={{ padding: "0"}} >
+                        <StyledLink to="/projectArchive">
+                            <ListItem button key={"Project Archive"} sx={{ paddingLeft: "32px" }}>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Project Archive" />
+                            </ListItem>
+                        </StyledLink>
+                    </AccordionDetails>
+                </StyledAccordion>
 
-                <AccordionDetails sx={{ padding: "0"}} >
-                    <StyledLink to="/projectArchive">
-                        <ListItem button key={"Project Archive"} sx={{ paddingLeft: "32px" }}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Project Archive" />
-                        </ListItem>
-                    </StyledLink>
-                </AccordionDetails>
-            </StyledAccordion>
+                {/* Nav Tickets */}
 
-            {/* Nav Tickets */}
+                <StyledAccordion 
+                    expanded={expanded === 'panel2'} 
+                    onChange={handleChange('panel2')} 
+                    disableGutters={true}
+                    elevation={0}
+                >
+                    <StyledAccordionSummary expandIcon={<ExpandMoreIcon />} >
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                            Tickets
+                        </Typography>
+                    </StyledAccordionSummary>
 
-            <StyledAccordion 
-                expanded={expanded === 'panel2'} 
-                onChange={handleChange('panel2')} 
-                disableGutters={true}
-                elevation={0}
-            >
-                <StyledAccordionSummary expandIcon={<ExpandMoreIcon />} >
-                    <ListItemIcon>
-                        <InboxIcon />
-                    </ListItemIcon>
-                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                        Tickets
-                    </Typography>
-                </StyledAccordionSummary>
+                    <AccordionDetails sx={{ padding: "0" }}>
+                        <StyledLink to="/allTickets">
+                            <ListItem button key={"All Tickets"} sx={{ paddingLeft: "32px" }}>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="All Tickets" />
+                            </ListItem>
+                        </StyledLink>
+                    </AccordionDetails>
 
-                <AccordionDetails sx={{ padding: "0" }}>
-                    <StyledLink to="/allTickets">
-                        <ListItem button key={"All Tickets"} sx={{ paddingLeft: "32px" }}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="All Tickets" />
-                        </ListItem>
-                    </StyledLink>
-                </AccordionDetails>
+                    <AccordionDetails sx={{ padding: "0" }}>
+                        <StyledLink to="/myTickets">
+                            <ListItem button key={"My Tickets"} sx={{ paddingLeft: "32px" }}>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="My Tickets" />
+                            </ListItem>
+                        </StyledLink>
+                    </AccordionDetails>
 
-                <AccordionDetails sx={{ padding: "0" }}>
-                    <StyledLink to="/myTickets">
-                        <ListItem button key={"My Tickets"} sx={{ paddingLeft: "32px" }}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="My Tickets" />
-                        </ListItem>
-                    </StyledLink>
-                </AccordionDetails>
+                    <AccordionDetails sx={{ padding: "0" }}>
+                        <StyledLink to="/addTicket">
+                            <ListItem button key={"Add Ticket"} sx={{ paddingLeft: "32px" }}>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Add Ticket" />
+                            </ListItem>
+                        </StyledLink>
+                    </AccordionDetails>
 
-                <AccordionDetails sx={{ padding: "0" }}>
-                    <StyledLink to="/addTicket">
-                        <ListItem button key={"Add Ticket"} sx={{ paddingLeft: "32px" }}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Add Ticket" />
-                        </ListItem>
-                    </StyledLink>
-                </AccordionDetails>
+                    <AccordionDetails sx={{ padding: "0" }}>
+                        <StyledLink to="/ticketArchive">
+                            <ListItem button key={"Ticket Archive"} sx={{ paddingLeft: "32px" }}>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Ticket Archive" />
+                            </ListItem>
+                        </StyledLink>
+                    </AccordionDetails>
 
-                <AccordionDetails sx={{ padding: "0" }}>
-                    <StyledLink to="/ticketArchive">
-                        <ListItem button key={"Ticket Archive"} sx={{ paddingLeft: "32px" }}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Ticket Archive" />
-                        </ListItem>
-                    </StyledLink>
-                </AccordionDetails>
+                </StyledAccordion>
+                
 
-            </StyledAccordion>
-            
+                {/* Nav User */}
 
-            {/* Nav User */}
+                <StyledAccordion 
+                    expanded={expanded === 'panel3'} 
+                    onChange={handleChange('panel3')} 
+                    disableGutters={true} 
+                    square={true} 
+                    elevation={0} 
+                >
+                    <StyledAccordionSummary expandIcon={<ExpandMoreIcon />} >
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                            Admin 
+                        </Typography>
+                    </StyledAccordionSummary>
 
-            <StyledAccordion 
-                expanded={expanded === 'panel3'} 
-                onChange={handleChange('panel3')} 
-                disableGutters={true} 
-                square={true} 
-                elevation={0} 
-            >
-                <StyledAccordionSummary expandIcon={<ExpandMoreIcon />} >
-                    <ListItemIcon>
-                        <InboxIcon />
-                    </ListItemIcon>
-                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                        Admin 
-                    </Typography>
-                </StyledAccordionSummary>
+                    <AccordionDetails sx={{ padding: "0" }}>
+                        <StyledLink to="/user">
+                            <ListItem button key={"Role"} sx={{ paddingLeft: "32px" }}>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Role" />
+                            </ListItem>
+                        </StyledLink>
+                    </AccordionDetails>
 
-                <AccordionDetails sx={{ padding: "0" }}>
-                    <StyledLink to="/user">
-                        <ListItem button key={"Role"} sx={{ paddingLeft: "32px" }}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Role" />
-                        </ListItem>
-                    </StyledLink>
-                </AccordionDetails>
+                    <AccordionDetails sx={{ padding: "0" }}>
+                        <StyledLink to="/user">
+                            <ListItem button key={"Role"} sx={{ paddingLeft: "32px" }}>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Role" />
+                            </ListItem>
+                        </StyledLink>
+                    </AccordionDetails>
+                </StyledAccordion>
+                <Divider />
+            </List>
 
-                <AccordionDetails sx={{ padding: "0" }}>
-                    <StyledLink to="/user">
-                        <ListItem button key={"Role"} sx={{ paddingLeft: "32px" }}>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Role" />
-                        </ListItem>
-                    </StyledLink>
-                </AccordionDetails>
-            </StyledAccordion>
-            <Divider />
-        </List>
-
-
-        {/* <Divider /> */}
         </div>
     )
 };

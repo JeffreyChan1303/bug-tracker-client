99c2013452b690c77ed83a5b590d51885dfaa289
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2022-07-13 17:15:38 -0400

    split ticketDetails.jsx and created ticketComments.jsx

--- src/components/Application/Tickets/ticketDetails.jsx
+++ src/components/Application/Tickets/ticketDetails.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { styled } from '@mui/material/styles';
+import React, { useEffect } from 'react';
 import {
   Typography,
   Paper,
@@ -9,43 +8,20 @@ import {
   Grid,
   Button,
   CircularProgress,
-  Box,
-  TextField,
-  Table,
-  TableHead,
-  TableRow,
-  TableBody,
-  TableCell,
-  IconButton,
-  Pagination,
-  PaginationItem,
 } from '@mui/material';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import DeleteIcon from '@mui/icons-material/Delete';
 import {
   getTicketDetails,
   moveTicketToArchive,
   deleteTicketFromArchive,
   restoreTicketFromArchive,
-  addTicketComment,
-  deleteTicketComment,
-  searchTicketComments,
 } from '../../../services/ticket/ticketDetailsSlice';
 
-import { handleAlerts } from '../../../services/alertsSlice';
 import TicketInformation from './ticketInformation';
-
-const BoldedTableCell = styled(TableCell)(({ theme }) => ({
-  fontWeight: theme.typography.fontWeightBold,
-  padding: '8px 5px',
-}));
-
-const ContentTableCell = styled(TableCell)(() => ({
-  padding: '5px',
-}));
+import TicketComments from './ticketComments';
 
 const getDateFromISODate = (ISODate) => {
   const date = new Date(ISODate);
@@ -55,34 +31,18 @@ const getDateFromISODate = (ISODate) => {
 
 const TicketDetails = () => {
   const { ticketId } = useParams();
-  const [comment, setComment] = useState('');
-  const [commentsSearch, setCommentsSearch] = useState('');
-  const [commentsCurrentPage, setCommentsCurrentPage] = useState(1);
-  // eslint-disable-next-line no-unused-vars
-  const [commentsItemsPerPage, setCommentsItemsPerPage] = useState(5);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const {
     getTicketDetails: { loading },
     ticket,
-    ticket: { ticketHistory, commentsNumberOfPages },
+    ticket: { ticketHistory },
   } = useSelector((state) => state.ticketDetails);
 
   useEffect(() => {
     dispatch(getTicketDetails(ticketId));
   }, []);
-
-  useEffect(() => {
-    // this searches based on searchquery, currentPage, and the number of entries per page
-    dispatch(
-      searchTicketComments({
-        searchQuery: commentsSearch,
-        commentsCurrentPage,
-        commentsItemsPerPage,
-      })
-    );
-  }, [commentsSearch, commentsCurrentPage, commentsItemsPerPage, ticket.comments]);
 
   // checks to see if ticket is a archived ticket or not
   const isArchived = ticket.status === 'Archived';
@@ -99,34 +59,6 @@ const TicketDetails = () => {
   const handleRecoverTicket = () => {
     dispatch(restoreTicketFromArchive(ticketId));
     navigate('/allTickets');
-  };
-
-  // WE NEED TO PUT THIS USER OBJECT IN A REDUX GLOBAL STATE. this will make retrieving some user information a lot easier
-  const userName = JSON.parse(localStorage.getItem('profile'))?.userObject?.name;
-
-  const handleSaveComment = () => {
-    const ticketComment = comment.trim();
-    if (ticketComment) {
-      dispatch(addTicketComment({ ticketId, comment: { name: userName, message: ticketComment } }));
-      dispatch(getTicketDetails(ticketId));
-      setComment('');
-    } else {
-      dispatch(handleAlerts({ severity: 'warning', message: 'Invalid Comment' }));
-    }
-  };
-  const handleAddCommentKeyPress = (e) => {
-    if (e.keyCode === 13) {
-      handleSaveComment();
-    }
-  };
-
-  const handleDeleteComment = (commentCreatedAt) => {
-    dispatch(deleteTicketComment({ ticketId, commentCreatedAt }));
-    dispatch(getTicketDetails(ticketId));
-  };
-
-  const handleCommentPageChange = (page) => {
-    setCommentsCurrentPage(page);
   };
 
   return loading ? (
@@ -194,94 +126,7 @@ const TicketDetails = () => {
 
       {/* This is the Ticket Comments Section */}
       <Grid item xs={12} lg={6}>
-        <Paper sx={{ p: 3 }} elevation={3}>
-          <Box sx={{ overflowX: 'scroll' }}>
-            <Grid container justifyContent="space-between">
-              <Typography variant="h6" fontWeight={700}>
-                Ticket Comments
-              </Typography>
-              <Box sx={{ display: 'flex', justifyContent: 'right' }}>
-                <Typography align="right" variant="body1">
-                  {' '}
-                  Search:&nbsp;{' '}
-                </Typography>
-                <TextField
-                  size="small"
-                  variant="standard"
-                  name="search"
-                  value={commentsSearch}
-                  onChange={(e) => setCommentsSearch(e.target.value)}
-                />
-              </Box>
-            </Grid>
-            <Table aria-label="simple table" size="small">
-              <TableHead>
-                <TableRow>
-                  <BoldedTableCell>User</BoldedTableCell>
-                  <BoldedTableCell align="left">Message</BoldedTableCell>
-                  <BoldedTableCell align="left">Created</BoldedTableCell>
-                  <BoldedTableCell align="center">Delete</BoldedTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {ticket.searchedComments?.map((ticketComment) => (
-                  <TableRow
-                    key={ticketComment.createdAt}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                    <ContentTableCell component="th" scope="row">
-                      {ticketComment.name}
-                    </ContentTableCell>
-                    <ContentTableCell align="left">{ticketComment.message}</ContentTableCell>
-                    <ContentTableCell align="left">
-                      {getDateFromISODate(ticketComment.createdAt)}
-                    </ContentTableCell>
-                    <ContentTableCell align="center">
-                      <IconButton onClick={() => handleDeleteComment(ticketComment.createdAt)}>
-                        <DeleteIcon />
-                      </IconButton>
-                    </ContentTableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Box>
-
-          <Pagination
-            sx={{ ul: { justifyContent: 'space-around' }, mt: '20px' }}
-            count={commentsNumberOfPages}
-            page={commentsCurrentPage}
-            variant="outlined"
-            color="primary"
-            renderItem={(item) => (
-              <PaginationItem
-                {...item}
-                component={Button}
-                onClick={() => handleCommentPageChange(item.page)}
-              />
-            )}
-          />
-        </Paper>
-
-        <Paper elevation={3} sx={{ p: 2, mt: 2 }}>
-          <Typography fontWeight={700} variant="body1" align="center" marginBottom={1}>
-            Add a comment?
-          </Typography>
-          <Grid container justifyContent="space-evenly">
-            <Grid item xs={8}>
-              <TextField
-                size="small"
-                variant="outlined"
-                fullWidth
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                onKeyDown={handleAddCommentKeyPress}
-              />
-            </Grid>
-            <Button variant="outlined" onClick={handleSaveComment}>
-              Save Comment
-            </Button>
-          </Grid>
-        </Paper>
+        <TicketComments ticketId={ticketId} getDateFromISODate={getDateFromISODate} />
       </Grid>
     </Grid>
   );

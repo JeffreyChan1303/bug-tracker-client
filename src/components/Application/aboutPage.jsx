a8543020a11c7e9150a9a0a999e87369441810e5
Jeffrey Chan <jeffreychan1303@gmail.com>
2024-04-06 15:59:49 -0400

    fix: update portfolio website link

--- src/components/Application/aboutPage.jsx
+++ src/components/Application/aboutPage.jsx
@@ -3,19 +3,19 @@ import { Container, Typography, Link, List, ListItem } from '@mui/material';
 
 const AboutPage = () => (
   <Container>
-    <Typography variant="h3" fontWeight={700} marginBottom={2}>
+    <Typography variant='h3' fontWeight={700} marginBottom={2}>
       About Me Page
     </Typography>
-    <Typography variant="h5">Hello, I am Jeffrey, the creator of this application.</Typography>
+    <Typography variant='h5'>Hello, I am Jeffrey, the creator of this application.</Typography>
 
     <List>
       <ListItem>
-        <Link target="_blank" href="https://jeffreychan1303.github.io/">
+        <Link target='_blank' href='https://jeffreychan.dev/'>
           Come check out my portfolio website
         </Link>
       </ListItem>
       <ListItem>
-        <Link target="_blank" href="https://github.com/JeffreyChan1303">
+        <Link target='_blank' href='https://github.com/JeffreyChan1303'>
           Come check out my Github
         </Link>
       </ListItem>
